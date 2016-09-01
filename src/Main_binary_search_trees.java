dc02bc0b2190e7f016ae34b481f067d2e3bb9760
Aleksandr <freezers@ukr.net>
2016-09-01 21:51:57 +0300

    added fixing tree

--- src/Main_binary_search_trees.java
+++ src/Main_binary_search_trees.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class Leaf {
@@ -25,6 +27,19 @@ class Leaf {
 	void addRight(int i) {
 		right = new Leaf(i);
 		right.parent = this;
+	}
+
+	void fixTree() {
+		ArrayList<Integer> list = Main_binary_search_trees.sort(inorderTreeWalk());
+		fixTreeUtil(list, this);
+	}
+
+	void fixTreeUtil(List<Integer> list, Leaf m) {
+		if (m != null) {
+			fixTreeUtil(list, m.left);
+			m.value = list.remove(0);
+			fixTreeUtil(list, m.right);
+		}
 	}
 
 	static Leaf createFromFile(String file) {
@@ -64,13 +79,42 @@ class Leaf {
 		}
 		return m;
 	}
+
+	ArrayList<Integer> inorderTreeWalk() {
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		inorderTreeWalkUtil(res, this);
+		return res;
+	}
+
+	void inorderTreeWalkUtil(List<Integer> list, Leaf m) {
+		if (m != null) {
+			inorderTreeWalkUtil(list, m.left);
+			list.add(m.value);
+			inorderTreeWalkUtil(list, m.right);
+		}
+	}
 }
 
 public class Main_binary_search_trees {
+	static ArrayList<Integer> sort(ArrayList<Integer> list) {
+		for (int i = 1; i < list.size(); i++)
+			for (int j = i - 1; j >= 0; j--) {
+				if (list.get(j + 1) < list.get(j)) {
+					int temp = list.get(j + 1);
+					list.set(j + 1, list.get(j));
+					list.set(j, temp);
+				}
+			}
+		return list;
+	}
 
 	public static void main(String[] args) {
 
 		Leaf leaf = Leaf.createFromFile("data_examples_07/input_10a.txt");
+		System.out.println(leaf.inorderTreeWalk());
+		leaf.fixTree();
+		System.out.println(leaf.inorderTreeWalk());
 		
+
 	}
 }
